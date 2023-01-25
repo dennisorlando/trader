17c39346917a4665d6d2c81db7349db581f3d55f
Dennis Orlando <dennis.orlando2002@gmail.com>
2023-01-25 13:00:26 +0100

    stuff

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,28 @@ pub mod trader;
 
 #[cfg(test)]
 mod tests {
-    
+    use std::borrow::Borrow;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
     use bfb::bfb_market::Bfb;
     use bose::market::BoseMarket;
     use dogemarket::dogemarket::DogeMarket;
     use market_common::market::Market;
-    use crate::trader::Trader;
-    
+
     use crate::trader::MarketKind::{BFB, BOSE, DOGE};
+    use crate::trader::Trader;
 
     #[test]
     fn trader_example() {
-
         let bose = BoseMarket::new_random();
         let doge = DogeMarket::new_random();
         let bfb = Bfb::new_random();
 
-        let trader = Trader::new(100_000)
-            .add_market(BFB, doge)
-            .add_market(DOGE, bfb)
-            .add_market(BOSE, bose);
+        let trader = Trader::new()
+            .with_market(BOSE, Rc::clone(&bose))
+            .with_market(DOGE, Rc::clone(&doge))
+            .with_market(BFB, Rc::clone(&bfb));
 
         trader.run(5);
         trader.run(5);
--- src/trader.rs
+++ src/trader.rs
@@ -1,51 +1,58 @@
-use std::cell::{RefCell};
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fmt::{Debug, Formatter};
 use std::rc::Rc;
+
 use market_common::good::good_kind::GoodKind;
 use market_common::good::good_kind::GoodKind::*;
 use market_common::market::Market;
 
+use crate::trader::MarketKind::{BFB, BOSE, DOGE};
+
 #[derive(Eq, Hash, PartialEq)]
+
+//this enum is utterly specific for our implementation and can't be generalized. Bad!
 pub enum MarketKind {
     BOSE,
     DOGE,
     BFB,
 }
 
-pub struct Trader
-{
-
+pub struct Trader<'a> {
     //todo: rename this field from "closure" to something else
-    closure: Box<dyn Fn()>,
+    closure: Box<dyn Fn() + 'a>,
 
     //market related fields
     owned_goods: HashMap<GoodKind, u32>,
 
     markets: HashMap<MarketKind, Rc<RefCell<dyn Market>>>,
-
 }
 
-//todo
-impl Debug for Trader {
+impl<'a> Debug for Trader<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        if f.alternate(){
-            return write!(f, "➤ Trader Status:\n • Markets: {}\n • Money: {}",
-                   self.markets.iter().map(|(_,t)| format!("\"{}\", ", (**t).borrow().get_name())  ).collect::<String>(),
-                   self.owned_goods.get(&EUR).unwrap()
+        if f.alternate() {
+            return write!(
+                f,
+                "➤ Trader Status:\n • Markets: {}\n • Money: {}",
+                self.markets
+                    .iter()
+                    .map(|(_, t)| format!("\"{}\", ", (**t).borrow().get_name()))
+                    .collect::<String>(),
+                self.owned_goods.get(&EUR).unwrap()
             );
         }
-        write!(f, "➤ Trader status: (Money: {})", self.owned_goods.get(&EUR).unwrap())
-
+        write!(
+            f,
+            "➤ Trader status: (Money: {})",
+            self.owned_goods.get(&EUR).unwrap()
+        )
     }
 }
 
-impl Trader {
-
-    pub fn new(initial_money: u32) -> Trader {
-
+impl<'a> Trader<'a> {
+    pub fn new() -> Self {
         let mut owned_goods = HashMap::new();
-        owned_goods.insert(EUR, initial_money);
+        owned_goods.insert(EUR, 1000);
         owned_goods.insert(USD, 0);
         owned_goods.insert(YEN, 0);
         owned_goods.insert(YUAN, 0);
@@ -53,24 +60,22 @@ impl Trader {
         Trader {
             closure: Box::new(|| {}),
             owned_goods,
-            markets: HashMap::new()
+            markets: HashMap::new(),
         }
     }
-    pub fn add_market(mut self, kind : MarketKind, market : Rc<RefCell<dyn Market>>) -> Trader {
+
+    pub fn with_market(mut self, kind: MarketKind, market: Rc<RefCell<dyn Market>>) -> Self {
         self.markets.insert(kind, market);
         self
     }
 
-    pub fn run(&self, iterations : u32){
+    pub fn run(&self, iterations: u32) {
         for _ in 0..iterations {
             (self.closure)();
         }
     }
 
-    //This supposes that the users create a static function instead of an actual closure
-    pub fn set_closure(&mut self, function : impl Fn() + 'static) {
+    pub fn set_closure(&mut self, function: impl Fn() + 'a) {
         self.closure = Box::new(function);
     }
-
-
 }
